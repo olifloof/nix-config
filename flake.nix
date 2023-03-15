997b932c3990954e7a01e73d659bbef13bc65c56
pinkcreeper100 <35699052+pinkcreeper100@users.noreply.github.com>
2023-03-15 17:38:23 +0000

    add stuff to flake.nix and create flake.lock

--- flake.nix
+++ flake.nix
@@ -1,10 +1,17 @@
 {
-  description = "My NixOS / nix-darwin / nixos-generators systems";
+  description = "My NixOS config";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    ironbar = {
+      url = "github:jakestanger/ironbar";
+    };
 
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     snowfall-lib = {
       url = "github:snowfallorg/lib";
       inputs.nixpkgs.follows = "nixpkgs";
