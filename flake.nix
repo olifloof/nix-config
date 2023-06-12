37c229b425cde0d079c892388b8beb0fac88081d
pinkcreeper100 <35699052+pinkcreeper100@users.noreply.github.com>
2023-06-12 17:31:56 +0100

    refactor nixpkgs input and add home-manager

--- flake.nix
+++ flake.nix
@@ -2,7 +2,16 @@
   description = "Overworld";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
 
     snowfall-lib = {
       url = "github:snowfallorg/lib";
