4713648c1f0bcdb99ff1c10105fb201c8bec7024
pinkcreeper100 <35699052+pinkcreeper100@users.noreply.github.com>
2023-06-15 14:02:01 +0100

    init: helix module

--- modules/apps/helix/default.nix
+++ modules/apps/helix/default.nix
@@ -9,7 +9,21 @@ in {
     enable = lib.mkEnableOption "Helix Editor";
   };
 
-  config =
-    lib.mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
+    programs.helix = {
+      enable = true;
+      settings = {
+        theme = "catppuccin_mocha";
+        editor = {
+          bufferline = "multiple";
+          color-modes = true;
+          cursorline = true;
+          soft-wrap.enable = true;
+        };
+        keys.normal = {
+          esc = ["collapse_selection" "keep_primary_selection"];
+        };
+      };
     };
+  };
 }
