8a3e73219b23222dffe79e276529a5bae7df53cb
pinkcreeper100 <35699052+pinkcreeper100@users.noreply.github.com>
2023-07-20 15:45:52 +0100

    update helix config

--- modules/apps/helix/default.nix
+++ modules/apps/helix/default.nix
@@ -1,6 +1,8 @@
 {
   lib,
   config,
+  inputs,
+  pkgs,
   ...
 }: let
   cfg = config.overworld.programs.helix;
@@ -10,18 +12,35 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    programs.helix = {
-      enable = true;
-      settings = {
-        theme = "catppuccin_mocha";
-        editor = {
-          bufferline = "multiple";
-          color-modes = true;
-          cursorline = true;
-          soft-wrap.enable = true;
+    programs = {
+      helix = {
+        enable = true;
+        settings = {
+          theme = "catppuccin_mocha";
+          editor = {
+            bufferline = "multiple";
+            color-modes = true;
+            completion-trigger-len = 1;
+            idle-timeout = 0;
+            cursorline = true;
+            soft-wrap.enable = true;
+          };
+          keys = {
+            normal = {
+              esc = ["collapse_selection" "keep_primary_selection"];
+            };
+          };
         };
-        keys.normal = {
-          esc = ["collapse_selection" "keep_primary_selection"];
+        languages = {
+          language = [
+            {
+              name = "nix";
+              formatter = {
+                command = "${lib.getExe pkgs.alejandra}";
+              };
+              config.nil.nix.flake.autoEvalInputs = true;
+            }
+          ];
         };
       };
     };
