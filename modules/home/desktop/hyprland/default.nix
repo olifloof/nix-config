3e98cb7f00d1e350f2b54b474070aca2e95a6312
pinkcreeper100 <35699052+pinkcreeper100@users.noreply.github.com>
2023-08-22 15:06:40 +0100

    hl module

---
+++ modules/home/desktop/hyprland/default.nix
@@ -0,0 +1,89 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  inherit (lib) mkIf mkEnableOption;
+  cfg = config.creeper.home.desktop.hyprland;
+in {
+  options.creeper.home.desktop.hyprland = {
+    enable = mkEnableOption "Hyprland";
+  };
+  config = mkIf cfg.enable {
+    wayland.windowManager.hyprland = {
+      enable = true;
+      extraConfig = ''
+        monitor=,preferred,auto,auto
+        input {
+            kb_layout = gb
+            kb_model = pc104
+            follow_mouse = 1
+        }
+        general {
+            gaps_in = 5
+            gaps_out = 5
+            border_size = 2
+            col.active_border = rgb(f5c2e7)
+            col.inactive_border = rgb(1e1e2e)
+            layout = dwindle
+        }
+        decoration {
+            rounding = 0
+        }
+        animations {
+            enabled = true
+            bezier = myBezier, 0.05, 0.9, 0.1, 1.05
+            animation = windows, 1, 7, myBezier
+            animation = windowsOut, 1, 7, default, popin 80%
+            animation = border, 1, 10, default
+            animation = borderangle, 1, 8, default
+            animation = fade, 1, 7, default
+            animation = workspaces, 1, 6, default
+        }
+        dwindle {
+            pseudotile = true
+            preserve_split = true
+        }
+        $mainMod = SUPER
+        bind = $mainMod, Q, exec, ${pkgs.wezterm}/bin/wezterm
+        bind = $mainMod, C, killactive,
+        bind = $mainMod, M, exit,
+        # bind = $mainMod, E, exec, dolphin
+        bind = $mainMod, V, togglefloating,
+        # bind = $mainMod, R, exec, wofi --show drun
+        bind = $mainMod, P, pseudo, # dwindle
+        bind = $mainMod, J, togglesplit, # dwindle
+        bind = $mainMod, left, movefocus, l
+        bind = $mainMod, right, movefocus, r
+        bind = $mainMod, up, movefocus, u
+        bind = $mainMod, down, movefocus, d
+        bind = $mainMod, 1, workspace, 1
+        bind = $mainMod, 2, workspace, 2
+        bind = $mainMod, 3, workspace, 3
+        bind = $mainMod, 4, workspace, 4
+        bind = $mainMod, 5, workspace, 5
+        bind = $mainMod, 6, workspace, 6
+        bind = $mainMod, 7, workspace, 7
+        bind = $mainMod, 8, workspace, 8
+        bind = $mainMod, 9, workspace, 9
+        bind = $mainMod, 0, workspace, 10
+        bind = $mainMod SHIFT, 1, movetoworkspace, 1
+        bind = $mainMod SHIFT, 2, movetoworkspace, 2
+        bind = $mainMod SHIFT, 3, movetoworkspace, 3
+        bind = $mainMod SHIFT, 4, movetoworkspace, 4
+        bind = $mainMod SHIFT, 5, movetoworkspace, 5
+        bind = $mainMod SHIFT, 6, movetoworkspace, 6
+        bind = $mainMod SHIFT, 7, movetoworkspace, 7
+        bind = $mainMod SHIFT, 8, movetoworkspace, 8
+        bind = $mainMod SHIFT, 9, movetoworkspace, 9
+        bind = $mainMod SHIFT, 0, movetoworkspace, 10
+        bind = $mainMod, mouse_down, workspace, e+1
+        bind = $mainMod, mouse_up, workspace, e-1
+        bindm = $mainMod, mouse:272, movewindow
+        bindm = $mainMod, mouse:273, resizewindow
+      '';
+      systemdIntegration = true;
+    };
+  };
+}
